4db66e9d80cc4660cc27e0a613103e8cca8c71e5
loicortola <loic.ortola@free.fr>
2012-10-12 11:21:03 +0200

    Loic

--- src/ws/GeoTrackerMenu.java
+++ src/ws/GeoTrackerMenu.java
@@ -29,10 +29,11 @@ public class GeoTrackerMenu
 	
 	public GeoTrackerMenu(Composite parent) throws RemoteException 
 	{
-		initLayout(parent);
+		
 		
 		LocGetIds myIds = new LocGetIds();	
 		_ids = myIds.show();
+		initLayout(parent);
 	}
 	
 	public void initLayout(Composite parent) throws RemoteException
