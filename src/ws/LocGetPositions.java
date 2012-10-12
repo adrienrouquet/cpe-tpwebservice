5775ffbaa63d4d442bf3f5c804b1bd27de3168d8
henrilahoud <lahoud.henri@gmail.com>
2012-10-12 09:34:22 +0200

    "BigTable" data implemented plug into GUI remaining

--- src/ws/LocGetPositions.java
+++ src/ws/LocGetPositions.java
@@ -9,41 +9,67 @@ import ws.GeoTrackerServiceStub.PositionLog;
 public class LocGetPositions {
 
 	private PositionLog[] _points = null;
+	
+	private String _iD = null;
+	private String _minDate = null;
+	private String _maxDate = null;
+	private int _maxResponse = 0;
     /**
      * @param args
      * @throws RemoteException
      */
     public LocGetPositions( String pId , String pMinDate, String pMaxDate, int pMaxResponse) throws RemoteException
     {
-        System.out.println("Retrieving route of vehicle " + pId );
-        // TODO Auto-generated method stub
-        // objet stub pour acceder au web service
+    	_iD = pId;
+    	_minDate = pMinDate;
+    	_maxDate = pMaxDate;
+    	_maxResponse = pMaxResponse;
+    	
+    	createLogs();
+    }
+    
+    private void createLogs() throws RemoteException
+    {
+    	// objet stub pour acceder au web service
         GeoTrackerServiceStub stub = new GeoTrackerServiceStub();
 
         // on prepare les objets messages
         GetPositions positions = new GetPositions();
-        positions.setId(pId);
-        positions.setMinDate(pMinDate);
-        positions.setMaxDate(pMaxDate);
-        positions.setMaxResponse(pMaxResponse);
+        positions.setId(_iD);
+        positions.setMinDate(_minDate);
+        positions.setMaxDate(_maxDate);
+        positions.setMaxResponse(_maxResponse);
         positions.setOnlyMovingPositions(false);
-
 
         GetPositionsResponse rPositions = new GetPositionsResponse();
         rPositions = stub.getPositions(positions);
 
         _points = rPositions.get_return();
-
-        for (int i = 0; i < Math.min(_points.length, pMaxResponse) ; i++)
-        {
-            System.out.println("Position " + (i+1));
-            System.out.println("Latitude:" + _points[i].getLatitude());
-            System.out.println("Longitude:" + _points[i].getLongitude());
-            System.out.println("Cap:" + _points[i].getHeading());
-            System.out.println("Date:" + _points[i].getDateString());
-            System.out.println();
-        }
-       
     }
-
+    
+    public double latitude(int pointNumber)
+    {
+    	PositionLog myPoint = _points[pointNumber];  	
+    	return myPoint.getLatitude();
+    }
+    public double longitude(int pointNumber)
+    {
+    	PositionLog myPoint = _points[pointNumber];
+    	return myPoint.getLongitude();
+    }
+    public int speed(int pointNumber)
+    {
+    	PositionLog myPoint = _points[pointNumber];
+    	return myPoint.getSpeed();
+    }
+    public int heading(int pointNumber)
+    {
+    	PositionLog myPoint = _points[pointNumber];
+    	return myPoint.getHeading();
+    }
+    public String date(int pointNumber)
+    {
+    	PositionLog myPoint = _points[pointNumber];
+    	return myPoint.getDateString();
+    }
 }
